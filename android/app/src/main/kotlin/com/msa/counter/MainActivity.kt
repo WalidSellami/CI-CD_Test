56c92545fbf5f953a0e8f0ee8a0af57ee926b9c4
WalidSellami <walid.sellami@zohomail.com>
2025-10-27 15:14:25 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/msa/counter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.msa.counter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Counter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+            primary: Colors.blue.shade700),
+      ),
+      themeMode: ThemeMode.light,
+      home: const MyHomePage(title: 'Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final ValueNotifier<int> counter = ValueNotifier<int>(0);
+
+  void _incrementCounter() {
+    counter.value++;
+  }
+
+  void _resetCounter() {
+    counter.value = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+            widget.title,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      body: Center(
+        child: ValueListenableBuilder(
+          valueListenable: counter,
+          builder: (context, value, child) {
+            return Text(
+              '${counter.value}',
+              style: TextStyle(
+                fontSize: 52.0,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            heroTag: 'increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _resetCounter,
+            tooltip: 'Reset',
+            heroTag: 'reset',
+            child: Text(
+              'Reset',
+              style: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
