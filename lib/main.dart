941ed0a77f6b2451c9a25e369b1962adba6f20fb
WalidSellami <walid.sellami@zohomail.com>
2025-10-28 17:39:33 +0100

    new way to deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:counter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
